97fd0cb6ca49b93e8dd5f6f49ff703bc1dc97f5a
cryptcoin-junkey <cryptcoin.junkey@gmail.com>
2020-09-22 07:19:30 +0000

    Add 'SPACEMONA' and 'MONANA' to quote assets.
    Signed-off-by: cryptcoin-junkey <cryptcoin.junkey@gmail.com>

--- counterblock/lib/config.py
+++ counterblock/lib/config.py
@@ -3,7 +3,7 @@
 ##
 # CONSTANTS
 ##
-VERSION = "1.4.0+MONA003"  # should keep up with counterblockd repo's release tag
+VERSION = "1.4.0+MONA004"  # should keep up with counterblockd repo's release tag
 
 DB_VERSION = 24  # a db version increment will cause counterblockd to rebuild its database off of counterpartyd
 
@@ -32,7 +32,7 @@ XCP_TO_BTC = XCP + '/' + BTC
 MAX_REORG_NUM_BLOCKS = 10  # max reorg we'd likely ever see
 MAX_FORCED_REORG_NUM_BLOCKS = 20  # but let us go deeper when messages are out of sync
 
-QUOTE_ASSETS = [BTC, XBTC, XCP]  # define the priority for quote asset
+QUOTE_ASSETS = [BTC, XBTC, XCP, 'SPACEMONA', 'MONANA']  # define the priority for quote asset
 MARKET_LIST_QUOTE_ASSETS = [XCP, XBTC, BTC]  # define the order in the market list
 
 DEFAULT_BACKEND_PORT_TESTNET = 19402
